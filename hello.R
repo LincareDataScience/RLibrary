80e21a67bad52b0abc28978aaa6a2c4132cb577a
JCeleste <jceleste@lincare.com>
2022-07-11 14:53:09 -0400

    fd

--- hello.R
+++ hello.R
@@ -14,7 +14,11 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 hello <- function() {
-  print("Hello, world....SYKE!")
+  print("Hello, world...JK_(hello)!")
+}
+
+test <- function(){
+  print("JOE")
 }
 
 
